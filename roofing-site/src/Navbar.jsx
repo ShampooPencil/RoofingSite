8e5f73cd21bf6b44b452b12a4b2f49c16b6ec639
Andrew Aramouni <aramouni8@gmail.com>
2025-09-20 18:13:57 -0400

    working on navbar

--- roofing-site/src/Navbar.jsx
+++ roofing-site/src/Navbar.jsx
@@ -1,12 +1,24 @@
 import apexMainPic from './assets/apexMainPic.png';
+import './Navbar.css';
 
 function Navbar() {
   return (
     <>
       <div className="navContainer">
         {/*Going to use <a href="apex main site" later on */}
-        <img src={apexMainPic} />
-        <div>APEX</div>
+        {/* <img src={apexMainPic} /> */}
+        <div>
+          <img className='mainLogo' src={apexMainPic} />
+        </div>
+        <div>
+          Home
+        </div>
+        <div>
+          Services
+        </div>
+        <div>
+          Contact Us
+        </div>
       </div>
 
     </>
